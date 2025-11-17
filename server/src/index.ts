8758b206e82a273acc3b512256dc70850abc96ab
Sa3fu <saifuden786@gmail.com>
2025-11-18 00:01:48 +0400

    Project skeleton (client + server)

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API running..." });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
